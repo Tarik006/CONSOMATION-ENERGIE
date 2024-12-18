2b63580c051be0750eba60cf94f414131df6f7ce
TARIK12306 <boudraa.tariik@gmail.com>
2024-12-18 23:09:38 +0100

    version 3

--- app/forms.py
+++ app/forms.py
@@ -2,9 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField, SubmitField, SelectField
 from wtforms.validators import DataRequired, NumberRange
 
+
 class APIForm(FlaskForm):
-    humidity = IntegerField('humidity', validators=[DataRequired(), NumberRange(min=18, max=99)])
-    temperature = IntegerField('Temperature', validators=[DataRequired(), NumberRange(min=18, max=99)])
-    zone = SelectField('Choisissez une zone', choices=[('PowerConsumption_Zone1', 'Zone 1'), ('PowerConsumption_Zone2', 'Zone 2'), ('PowerConsumption_Zone3', 'Zone 3')], validators=[DataRequired()])
+    humidity = IntegerField('humidity', validators=[DataRequired(), NumberRange(min=0, max=100)])
+    temperature = IntegerField('Temperature', validators=[DataRequired(), NumberRange(min=0, max=45)])
+    zone = SelectField('Choisissez une zone', choices=[('PowerConsumption_Zone1', 'Zone 1'), ('PowerConsumption_Zone2', 'Zone 2'), ('PowerConsumption_Zone3', 'Zone 3')],validators=[DataRequired()] )
 
     submit = SubmitField('Valider')
